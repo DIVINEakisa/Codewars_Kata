3cb106034dbc8c981c63fdef670728dad0b257f2
DIVINEakisa <akisadivine11@gmail.com>
2025-11-05 17:08:44 +0200

    Lazy Repeater

---
+++ 5 kyu/makeLooper.js
@@ -0,0 +1,22 @@
+// Description:
+// The makeLooper() function (or make_looper in your language) takes a string (of non-zero length) as an argument. It returns a function. The function it returns will return successive characters of the string on successive invocations. It will start back at the beginning of the string once it reaches the end.
+
+// For example:
+
+// const abc = makeLooper('abc');
+// abc(); // should return 'a' on this first call
+// abc(); // should return 'b' on this second call
+// abc(); // should return 'c' on this third call
+// abc(); // should return 'a' again on this fourth call
+// Different loopers should not affect each other, so be wary of unmanaged global state.
+
+// Iterators
+
+function makeLooper(str) {
+  let index = 0;
+  return function () {
+    let char = str[index];
+    index = (index + 1) % str.length;
+    return char;
+  };
+}
