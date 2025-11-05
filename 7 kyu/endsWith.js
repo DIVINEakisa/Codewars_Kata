891a4aeab62207d6b875fe83ae920feb347b23e4
DIVINEakisa <akisadivine11@gmail.com>
2025-11-05 17:14:09 +0200

    String ends with?

---
+++ 7 kyu/endsWith.js
@@ -0,0 +1,17 @@
+// Description:
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// Inputs: "abc", "bc"
+// Output: true
+
+// Inputs: "abc", "d"
+// Output: false
+
+function solution(str, ending) {
+  // TODO: complete
+  return str.endsWith(ending);
+}
+console.log(solution("abc", "bc"));
+console.log(solution("abc", "d"));
