bc6a67728d7a9a6759a0145cbb1f402fb6d5a2fb
DIVINEakisa <akisadivine11@gmail.com>
2025-11-07 20:29:55 +0200

    get ascii value of character

---
+++ 8 kyu/getascii.js
@@ -0,0 +1,10 @@
+// Description:
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+function getASCII(c) {
+  // ...
+  let text = c.charCodeAt();
+  return text;
+}
