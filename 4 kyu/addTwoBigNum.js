71e5397bf7afaedcba6e5fdf00c4b16bbc5ea7f3
DIVINEakisa <akisadivine11@gmail.com>
2026-01-27 17:10:42 +0200

    Answer

--- 4 kyu/addTwoBigNum.js
+++ 4 kyu/addTwoBigNum.js
@@ -10,3 +10,23 @@
 // The input is a string of only digits
 // The numbers are positives
 
+function add(a, b) {
+  let carry = 0;
+  let result = '';
+
+  a = a.split('').reverse();
+  b = b.split('').reverse();
+
+  const max = Math.max(a.length, b.length);
+
+  for (let i = 0; i < max; i++) {
+    const sum = (Number(a[i] || 0) + Number(b[i] || 0) + carry);
+    result = (sum % 10) + result;
+    carry = Math.floor(sum / 10);
+  }
+
+  if (carry) result = carry + result;
+
+  return result;
+}
+console.log(add("123", "321")); // "444"
