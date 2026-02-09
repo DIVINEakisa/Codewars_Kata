e37c099da51c6ee2e643c07768e93b2a10fcb9e2
DIVINEakisa <akisadivine11@gmail.com>
2026-02-09 11:52:35 +0200

    Terminal game combat function Solved

--- TS/8kyu/terminalGame.ts
+++ TS/8kyu/terminalGame.ts
@@ -1,1 +1,8 @@
 // Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
+export function combat(health: number, damage: number): number {
+  if (health <= damage) {
+    return 0;
+  }
+
+  return health - damage;
+}
