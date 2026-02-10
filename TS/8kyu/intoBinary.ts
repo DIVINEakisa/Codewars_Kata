e09ee1188931646112ef4343c362563d9de1900b
DIVINEakisa <akisadivine11@gmail.com>
2026-02-10 15:55:18 +0200

    One and Zeros Answered

--- TS/8kyu/intoBinary.ts
+++ TS/8kyu/intoBinary.ts
@@ -13,3 +13,7 @@
 // Testing: [1, 1, 1, 1] ==> 15
 // Testing: [1, 0, 1, 1] ==> 11
 // However, the arrays can have varying lengths, not just limited to 4.
+
+export function binaryArrayToNumber(arr: number[]): number {
+  return parseInt(arr.join(""), 2);
+}
