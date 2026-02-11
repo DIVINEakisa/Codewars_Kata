554e8f75774c8d6f0e88e13e6f4c4f34da645860
DIVINEakisa <akisadivine11@gmail.com>
2026-02-11 20:26:09 +0200

    A Promise is a Promise

---
+++ Asynchronous/promise.js
@@ -0,0 +1,6 @@
+// Create a resolved javascript Promise that will return 'Hello World!'.
+// function promiseHelloWorld() {
+//   // replace this nonsense with your Promise
+
+//   return / / ? '' : (() => 'Hello World!')();
+// }
