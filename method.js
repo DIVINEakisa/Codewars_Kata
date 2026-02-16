3424cd43ab3954c2a3a270c4006196ba964c9596
DIVINEakisa <akisadivine11@gmail.com>
2026-02-16 14:34:59 +0200

    Add New KYU

---
+++ method.js
@@ -0,0 +1,14 @@
+// Javascript Strings endsWith Method and StartWith Method
+// The startsWith() method determines whether a string begins with the characters of a specified string, returning true or false as appropriate.
+// Syntax
+// str.startsWith(searchString[, position])
+// The startsWith() method returns true if a string begins with a specified string.
+// let text = "Hello world, welcome to the universe.";
+// console.log(text.startsWith("Hello"));
+
+// The endsWith() method determines whether a string ends with the characters of a specified string, returning true or false as appropriate.
+// Syntax
+// str.endsWith(searchString[, length])
+//The endsWith() method returns true if a string ends with a specified string.
+let text1 = "Hello world, welcome to the universe.";
+console.log(text1.endsWith("universe."));
