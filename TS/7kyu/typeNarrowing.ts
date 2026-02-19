c03abfdd18b009b67448b599bf7331c16ed9c30b
DIVINEakisa <akisadivine11@gmail.com>
2026-02-19 18:20:10 +0200

    Typescript Type Narrowing

---
+++ TS/7kyu/typeNarrowing.ts
@@ -0,0 +1,3 @@
+// The new intern needs help! He has been learning Typescript for the first time, but is having trouble convincing the compiler that his types are ok. Can you help him make his code compile?
+
+// Making the code typecheck and compile correctly is all you need to do to pass this kata. The full tests are identical to the sample tests.
