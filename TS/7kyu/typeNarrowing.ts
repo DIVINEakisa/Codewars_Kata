5d63be92e443084ac545f1638a872431c7116b22
DIVINEakisa <akisadivine11@gmail.com>
2026-02-19 18:21:53 +0200

    Desc

--- TS/7kyu/typeNarrowing.ts
+++ TS/7kyu/typeNarrowing.ts
@@ -1,3 +1,9 @@
 // The new intern needs help! He has been learning Typescript for the first time, but is having trouble convincing the compiler that his types are ok. Can you help him make his code compile?
 
 // Making the code typecheck and compile correctly is all you need to do to pass this kata. The full tests are identical to the sample tests.
+// export function isPair(arr: any[]): boolean {
+//   return arr.length === 2;
+// }
+export function isPair(arr: any[]): arr is [any, any] {
+  return arr.length === 2;
+}
