f0b7231c3ef971479995be636a3b4cbb5bfb8d8b
DIVINEakisa <akisadivine11@gmail.com>
2026-02-21 20:42:26 +0200

    Syntax

--- JS/nulllish.js
+++ JS/nulllish.js
@@ -1,1 +1,6 @@
 //The  nullish operator returns the right-hand value only if the left-hand value is null or undefined
+// let result = value ?? defaultValue;
+let name = null;
+let displayName = name ?? "Guest";
+
+console.log(displayName);
