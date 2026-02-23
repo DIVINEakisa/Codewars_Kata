bb3650ea3ef4c8b05d99eb0ed1481e7f9a162d4d
DIVINEakisa <akisadivine11@gmail.com>
2026-02-23 08:52:54 +0200

    Sum Of Parts Answers

--- JS/6 kyu/sumOfParts.js
+++ JS/6 kyu/sumOfParts.js
@@ -23,3 +23,18 @@
 // Notes
 // Take a look at performance: some lists have thousands of elements.
 // Please ask before translating.
+function partsSums(ls) {
+  let result = [];
+  let total = ls.reduce((a, b) => a + b, 0);
+
+  result.push(total);
+
+  for (let i = 0; i < ls.length; i++) {
+    total -= ls[i];
+    result.push(total);
+  }
+
+  return result;
+}
+
+console.log(partsSums([0, 1, 3, 6, 10]));
