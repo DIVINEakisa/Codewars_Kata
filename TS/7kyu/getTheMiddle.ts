4497c9d6f38012147c9f0c1830c792b4925c72ff
DIVINEakisa <akisadivine11@gmail.com>
2026-03-06 13:47:17 +0200

    Get the middle character

--- TS/7kyu/getTheMiddle.ts
+++ TS/7kyu/getTheMiddle.ts
@@ -1,3 +1,12 @@
+// You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+
+// If the string's length is odd, return the middle character.
+// If the string's length is even, return the middle 2 characters.
+// Examples:
+// "test" --> "es"
+// "testing" --> "t"
+// "middle" --> "dd"
+// "A" --> "A"
 export function getMiddle(s: string) {
   let len = s.length;
   if (len % 2 === 0) {
