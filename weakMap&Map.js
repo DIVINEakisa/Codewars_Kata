955839c634aafa7c2390efb95d61cf9d8d679552
DIVINEakisa <akisadivine11@gmail.com>
2026-03-07 13:47:20 +0200

    Map Example

--- weakMap&Map.js
+++ weakMap&Map.js
@@ -1,2 +1,10 @@
 //Map is built in JS object that stores key-value pairs, where both keys and  values can be any datatype.
 //WeakMap is a special kind of Map that allows you to store key-value pairs where the keys must be objects and those keys are held weakly.
+// ex;
+// let myMap = new Map();
+// myMap.set("name", "Dede");
+// myMap.set("age", 20);
+
+// console.log(myMap.get("name"));
+// console.log(myMap.has("age"));
+// console.log(myMap.size);
