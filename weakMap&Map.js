c913f97b47163fe7c9baf1a0ffadcb92825dd947
DIVINEakisa <akisadivine11@gmail.com>
2026-03-07 13:46:17 +0200

    WeakMap and Map

---
+++ weakMap&Map.js
@@ -0,0 +1,2 @@
+//Map is built in JS object that stores key-value pairs, where both keys and  values can be any datatype.
+//WeakMap is a special kind of Map that allows you to store key-value pairs where the keys must be objects and those keys are held weakly.
