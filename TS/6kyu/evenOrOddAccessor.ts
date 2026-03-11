61b3368bbccda4cb777b0403082a54891eb72e2b
DIVINEakisa <akisadivine11@gmail.com>
2026-03-11 11:17:11 +0200

    Even or Odd Accessor Description

---
+++ TS/6kyu/evenOrOddAccessor.ts
@@ -0,0 +1,8 @@
+// Create a function or callable object that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. The function should also return "Even" or "Odd" when accessing a value at an integer index.
+
+// For example:
+
+// evenOrOdd(2); //'Even'
+// evenOrOdd[2]; //'Even'
+// evenOrOdd(7); //'Odd'
+// evenOrOdd[7]; //'Odd'
