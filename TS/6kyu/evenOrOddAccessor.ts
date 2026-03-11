03888395d309cad41d691d0b03812bc0fcf9c952
DIVINEakisa <akisadivine11@gmail.com>
2026-03-11 12:10:18 +0200

    Even or Odd Accessor Solution

--- TS/6kyu/evenOrOddAccessor.ts
+++ TS/6kyu/evenOrOddAccessor.ts
@@ -6,3 +6,19 @@
 // evenOrOdd[2]; //'Even'
 // evenOrOdd(7); //'Odd'
 // evenOrOdd[7]; //'Odd'
+const fn = (n: number) => {
+  if (n % 2 === 0) return "Even";
+  return "Odd";
+};
+export const evenOrOdd = new Proxy(fn, {
+  get(target, prop) {
+    let num = Number(prop);
+    if (num % 2 !== 0) {
+      return "Odd";
+    } else {
+      return "Even";
+    }
+  },
+});
+console.log(evenOrOdd(12));
+console.log(evenOrOdd(3));
