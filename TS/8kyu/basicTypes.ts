a3f67865be6877be3e42df88cbf1b2602cff60ef
DIVINEakisa <akisadivine11@gmail.com>
2026-03-24 10:44:33 +0200

    Learning TypeScript. Basic Types

--- TS/8kyu/basicTypes.ts
+++ TS/8kyu/basicTypes.ts
@@ -28,3 +28,36 @@
 // Export var14Undefined variable with type and value undefined.
 // Never
 // Export var15NeverFunction function that returns never value.
+
+export var var1Boolean: boolean = true;
+export var var2Decimal: number = 13;
+export var var3Hex: number = 0xf00d;
+export var var4Binary: number = 0b111111;
+export var var5Octal: number = 0o744;
+export var var6String: string = "Hello, world!";
+export var var7Array: any[] = [1, "test", { a: 3 }, 4, 5];
+export var var8NumericArray: number[] = [1, 2, 3, 4, 5];
+export var var9Tuple: [string, number] = ["key", 12345];
+
+export enum Color {
+  Red = 1,
+  Green = 2,
+  Blue = 4,
+}
+
+export var var10Enum: Color = Color.Blue;
+export var var11ArrayOfAny: Array<any> = [1, "test", { a: 3 }, 4, 5];
+export function var12VoidFunction(): void {
+  console.log("This function returns nothing");
+}
+
+export var var13Null: null = null;
+export var var14Undefined: undefined = undefined;
+
+export function var15NeverFunction(): never {
+  throw new Error("This function never returns");
+}
+
+console.log(var1Boolean);
+console.log(var2Decimal);
+console.log(var3Hex);
