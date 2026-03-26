887fe466bcca3555ce70e86963acc66ecd291298
DIVINEakisa <akisadivine11@gmail.com>
2026-03-26 10:55:35 +0200

    Multiplication table Answe

--- TS/6kyu/multiplicationTable.ts
+++ TS/6kyu/multiplicationTable.ts
@@ -8,3 +8,18 @@
 // For the given example, the return value should be:
 
 // [[1,2,3],[2,4,6],[3,6,9]]
+
+export function multiplicationTable(size: number): number[][] {
+  let result: number[][] = [];
+  for (let i = 1; i <= size; i++) {
+    let row: number[] = [];
+    for (let j = 1; j <= size; j++) {
+      let calculate: number = i * j;
+      row.push(calculate);
+    }
+    result.push(row);
+  }
+  return result;
+}
+console.log(multiplicationTable(3));
+console.log(multiplicationTable(4));
