3fe50f073ffdfb011a448a90d18b28e842ca9df4
DIVINEakisa <akisadivine11@gmail.com>
2026-04-29 10:35:23 +0200

    Solution of finding the missing letter kata on 6 kyu

---
+++ JS/6 kyu/findMissingLetter.js
@@ -0,0 +1,26 @@
+// Find the missing letter
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+
+function findMissingLetter(array) {
+  let previousLetter;
+  let currentLetter;
+  let skippedLetter;
+  for (let i = 1; i < array.length; i++) {
+    currentLetter = array[i].charCodeAt();
+    previousLetter = array[i - 1].charCodeAt();
+    if (currentLetter - previousLetter === 2) {
+      skippedLetter = previousLetter + 1;
+    }
+  }
+  return String.fromCharCode(skippedLetter);
+}
+console.log(findMissingLetter(["a", "b", "c", "d", "f"]));
+console.log(findMissingLetter(["O", "Q", "R", "S"]));
