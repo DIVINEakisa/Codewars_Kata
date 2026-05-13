d2983ad0fbf79c276ac007c85607686d2f887bac
DIVINEakisa <akisadivine11@gmail.com>
2026-05-13 10:58:44 +0200

    Vowel Count

--- JS/7 kyu/vowelCount.js
+++ JS/7 kyu/vowelCount.js
@@ -3,3 +3,11 @@
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 // The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  let vowel = ["a", "e", "i", "o", "u"];
+  let strArray = str.split("");
+  let res = strArray.filter((st) => vowel.includes(st));
+  return res.length;
+}
+console.log(getCount("abracadabra"));
