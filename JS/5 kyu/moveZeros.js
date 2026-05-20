ebc348bc57755597d343962ba1e0d135c374e704
DIVINEakisa <akisadivine11@gmail.com>
2026-05-20 15:18:48 +0200

    Moving Zeros To The End

--- JS/5 kyu/moveZeros.js
+++ JS/5 kyu/moveZeros.js
@@ -1,3 +1,18 @@
 // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function moveZeros(arr) {
+  let originalLength = arr.length;
+  for (let i = 0; i < originalLength; i++) {
+    if (arr[i] === 0) {
+      arr.push(arr[i]);
+      arr.splice(i, 1);
+      i--;
+      originalLength--;
+    }
+  }
+  return arr;
+}
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
+console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
