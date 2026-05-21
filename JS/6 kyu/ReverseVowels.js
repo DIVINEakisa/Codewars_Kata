06ec15f9f2e3acf75c5e3221499b8fb5ab4605fc
DIVINEakisa <akisadivine11@gmail.com>
2026-05-21 20:14:57 +0200

    Reverse Vowels In A String

--- JS/6 kyu/ReverseVowels.js
+++ JS/6 kyu/ReverseVowels.js
@@ -4,3 +4,23 @@
 // "Tomatoes" => "Temotaos"
 // "Reverse Vowels In A String" => "RivArsI Vewols en e Streng"
 // For simplicity, you can treat the letter y as a consonant, not a vowel.
+
+function reverseVowels(str) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let chars = str.split("");
+  let found = [];
+
+  for (let i = 0; i < chars.length; i++) {
+    if (vowels.includes(chars[i].toLowerCase())) {
+      found.push(chars[i]);
+    }
+  }
+
+  for (let i = 0; i < chars.length; i++) {
+    if (vowels.includes(chars[i].toLowerCase())) {
+      chars[i] = found.pop();
+    }
+  }
+
+  return chars.join("");
+}
